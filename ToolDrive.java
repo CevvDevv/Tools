c9da5d5a23173c86ab8a494ddf27b4c66c45ef5d
Cesar Moran <cesardevv@gmail.com>
2025-04-09 18:40:14 -0500

    1.0

---
+++ ToolDrive.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+public class ToolDrive {
+    public static void main (String[] args)
+    {
+        Hammer ham = new Hammer();
+        Wrench wrench = new Wrench();
+        Drill drill = new Drill();
+
+        ham.setName("Hammer");
+        wrench.setName("Wrench");
+        drill.setName("Drill");
+        
+        Toolbox<Tool> newBox = new Toolbox<>();
+
+        Toolbox<Tool> boxList = newBox;
+
+        newBox.add(ham);
+        newBox.add(wrench);
+        newBox.add(drill);
+
+        boxList.listTools(newBox.box);
+        System.out.println();
+        boxList.useToolBox(newBox.box);
+
+    }
+}
+
+abstract class Tool {
+    private String name = "";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public abstract void use();
+}
+
+class Hammer extends Tool {
+    public void use(){
+        System.out.println("Knocking with the Hammer.");
+    }
+}
+
+class Drill extends Tool {
+    public void use(){
+        System.out.println("Drilling with the Drill.");
+    }
+}
+
+class Wrench extends Tool {
+    public void use(){
+        System.out.println("Wrenching with the Wrench.");
+    }
+}
+
+class Toolbox<T extends Tool> {
+    List<T> box = new ArrayList<>();
+
+    public void add(T tool){
+        box.add(tool);
+    }
+
+    public void listTools(List<T> tools){
+        for (T tool : tools)
+        {
+            String name = tool.getName();
+            System.out.println(name);
+        }
+    }
+
+    public void useToolBox(List<T> tools){
+        for (T tool : tools)
+        {
+            tool.use();
+        }
+    }
+}
